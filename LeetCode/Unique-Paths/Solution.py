9e9c6c33d8529a85df4dc6cb37268e3fd75d03cd
K GAGAN ROHITH <161939027+Gagan-Rohith-16@users.noreply.github.com>
2026-03-09 10:01:09 +0530

    Add solution for Unique Paths

--- LeetCode/Unique-Paths/Solution.py
+++ LeetCode/Unique-Paths/Solution.py
@@ -1,14 +1,13 @@
 1class Solution:
 2    def uniquePaths(self, m: int, n: int) -> int:
 3        list=[[0 for i in range(n)] for j in range(m)]
-4        i,j=0,0
-5        for i in range(0,n):
-6            list[0][i]=1
-7        for i in range(0,m):
-8            list[i][0]=1
-9        for i in range(1,m):
-10            for j in range(1,n):
-11                list[i][j]=list[i][j-1]+list[i-1][j]
-12        return list[i][j]
-13
-14        
+4        for i in range(0,n):
+5            list[0][i]=1
+6        for i in range(0,m):
+7            list[i][0]=1
+8        for i in range(1,m):
+9            for j in range(1,n):
+10                list[i][j]=list[i][j-1]+list[i-1][j]
+11        return list[m-1][n-1]
+12
+13        
